9e18cda2c8bd4d146809b073ab119a4ef5d25677
Marco Li <marco_li@paradise-soft.com.tw>
2020-10-22 10:08:38 +0800

    重構 HTTPRequestHelper

--- mat_server/infrastructure/helpers/http_request_helper.py
+++ mat_server/infrastructure/helpers/http_request_helper.py
@@ -5,6 +5,9 @@ from mat_server.domain import helpers, entities
 
 class HTTPRequestHelper(helpers.HTTPRequestHelperBase):
 
+    def __init__(self):
+        self._session = requests.Session()
+
     def send(self, request: entities.HTTPRequest) -> entities.HTTPResponse:
         req = requests.Request(
             method=request.method,
@@ -12,14 +15,13 @@ class HTTPRequestHelper(helpers.HTTPRequestHelperBase):
             headers=request.headers,
             data=request.raw_body,
         )
-        resp = requests.Session().send(req.prepare(), stream=True)
-
-        headers = {}
-        for name, value in resp.headers.items():
-            headers[name.lower()] = value
+        resp = self._session.send(req.prepare(), stream=True)
 
         return entities.HTTPResponse(
             raw_data=resp.raw.read(),
             status_code=resp.status_code,
-            headers=headers,
+            headers={
+                name.lower(): value
+                for name, value in resp.headers.items()
+            },
         )
